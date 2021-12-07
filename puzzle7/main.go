d919fda6d40e98abe22532d35d94f03afb4bc453
Kevin Nguyen <kevioke1337@gmail.com>
2021-12-06 23:20:06 -0800

    Finish puzzle 7 part 2

--- puzzle7/main.go
+++ puzzle7/main.go
@@ -30,6 +30,18 @@ func absDiff(a, b int) int {
 	return b - a
 }
 
+func sumN(n int) int {
+	return n * (n + 1) / 2
+}
+
+func fuelCost2(crabPosCount map[int]int, candidatePos int) int {
+	cost := 0
+	for pos, numCrabs := range crabPosCount {
+		cost += sumN(absDiff(pos, candidatePos)) * numCrabs
+	}
+	return cost
+}
+
 func fuelCost(crabPosCount map[int]int, candidatePos int) int {
 	cost := 0
 	for pos, numCrabs := range crabPosCount {
@@ -41,7 +53,11 @@ func fuelCost(crabPosCount map[int]int, candidatePos int) int {
 func part1(crabPositions []int) int {
 	// generate map of crab position counts
 	crabPosCount := map[int]int{}
+	maxCrabPos := 0
 	for _, crabPos := range crabPositions {
+		if crabPos > maxCrabPos {
+			maxCrabPos = crabPos
+		}
 		if _, found := crabPosCount[crabPos]; !found {
 			crabPosCount[crabPos] = 1
 		} else {
@@ -51,7 +67,7 @@ func part1(crabPositions []int) int {
 
 	// for each crab position calculate difference and sum them keeping track of min
 	minCost := -1
-	for pos := range crabPosCount {
+	for pos := 0; pos < maxCrabPos; pos++ {
 		// first cost
 		if minCost == -1 {
 			minCost = fuelCost(crabPosCount, pos)
@@ -59,6 +75,39 @@ func part1(crabPositions []int) int {
 		}
 
 		cost := fuelCost(crabPosCount, pos)
+		if cost < minCost {
+			minCost = cost
+		}
+	}
+
+	return minCost
+}
+
+func part2(crabPositions []int) int {
+	// generate map of crab position counts
+	crabPosCount := map[int]int{}
+	maxCrabPos := 0
+	for _, crabPos := range crabPositions {
+		if crabPos > maxCrabPos {
+			maxCrabPos = crabPos
+		}
+		if _, found := crabPosCount[crabPos]; !found {
+			crabPosCount[crabPos] = 1
+		} else {
+			crabPosCount[crabPos]++
+		}
+	}
+
+	// for each crab position calculate difference and sum them keeping track of min
+	minCost := -1
+	for pos := 0; pos < maxCrabPos; pos++ {
+		// first cost
+		if minCost == -1 {
+			minCost = fuelCost2(crabPosCount, pos)
+			continue
+		}
+
+		cost := fuelCost2(crabPosCount, pos)
 		if cost < minCost {
 			minCost = cost
 		}
@@ -80,4 +129,5 @@ func main() {
 	}
 
 	fmt.Println(part1(numbers))
+	fmt.Println(part2(numbers))
 }
