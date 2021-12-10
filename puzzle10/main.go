e15e3e0ce8449a0ce8368e515c8b71fc3cb8b500
Kevin Nguyen <kevioke1337@gmail.com>
2021-12-10 08:20:08 -0800

    Puzzle 10 part 2

--- puzzle10/main.go
+++ puzzle10/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sort"
 )
 
 func readInput(r io.Reader) ([]string, error) {
@@ -44,6 +45,20 @@ func errorValue(a rune) int {
 	return 0
 }
 
+func completionValue(a rune) int {
+	switch a {
+	case ')':
+		return 1
+	case ']':
+		return 2
+	case '}':
+		return 3
+	case '>':
+		return 4
+	}
+	return 0
+}
+
 func isOpen(r rune) bool {
 	switch r {
 	case '(', '[', '{', '<':
@@ -60,18 +75,80 @@ func errorPos(line string) int {
 			continue
 		}
 
-		potentialMatch, empty := s.pop()
-		// missing data
+		openChar, empty := s.pop()
 		if empty {
-			return -1
+			panic("not enough open characters")
 		}
 
 		// mismatch
-		if !isMatching(potentialMatch, char) {
+		if !isMatching(openChar, char) {
 			return i
 		}
 	}
 	return -1
+}
+
+func pairClose(r rune) rune {
+	switch r {
+	case '(':
+		return ')'
+	case '[':
+		return ']'
+	case '{':
+		return '}'
+	case '<':
+		return '>'
+	}
+	return 0
+}
+
+func getClosingChars(line string) string {
+	var closingChars string
+	s := stack{}
+	for _, char := range line {
+		if isOpen(char) {
+			s.push(char)
+			continue
+		}
+
+		openChar, empty := s.pop()
+		if empty {
+			panic("not enough open characters")
+		}
+
+		if !isMatching(openChar, char) {
+			return ""
+		}
+	}
+
+	for {
+		char, empty := s.pop()
+		if empty {
+			break
+		}
+
+		closingChars = closingChars + string(pairClose(char))
+	}
+
+	return closingChars
+}
+
+func part2(lines []string) int {
+	points := []int{}
+	for _, line := range lines {
+		closingChars := getClosingChars(line)
+		if len(closingChars) > 0 {
+			sum := 0
+			for _, char := range closingChars {
+				sum *= 5
+				sum += completionValue(char)
+			}
+			points = append(points, sum)
+		}
+	}
+
+	sort.Ints(points)
+	return points[len(points)/2]
 }
 
 func part1(lines []string) int {
@@ -96,5 +173,6 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println(part1(lines))
+	//fmt.Println(part1(lines))
+	fmt.Println(part2(lines))
 }
